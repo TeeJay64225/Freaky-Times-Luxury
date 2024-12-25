3180b6f9feaed70443915d674757574e2f271347
Theophilus Joseph Quarm <a0240064225@Theophilus-Joseph-Quarms-MacBook-Air.local>
2024-12-25 11:01:38 +0000

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// hamburger = document.querySelector(".hamburger")
-// hamburger.onclick = function() {
-//     navbar = document.querySelector(".nav-bar")
-//     navbar.classList.toggle("active")
-// }
+ hamburger = document.querySelector(".hamburger")
+ hamburger.onclick = function() {
+     navbar = document.querySelector(".nav-bar")
+    navbar.classList.toggle("active")
+ }
